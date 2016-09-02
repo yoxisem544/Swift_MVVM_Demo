3cce5b88727c1f408f3037e62192cefeb73dc573
David Lin <yoxisem544@gmail.com>
2016-09-02 18:41:13 +0800

    Initial Commit

---
+++ MVVM_Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVVM_Demo
+//
+//  Created by David on 2016/9/2.
+//  Copyright © 2016年 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
